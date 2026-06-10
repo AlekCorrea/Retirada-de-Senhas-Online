c4572bd3613ce5107bc941ac5d0865872625eadf
Everton Serighelli <everton.serighelli@gmail.com>
2026-06-09 21:51:04 -0300

    figmas add

---
+++ figma/atendente/atendente.vue
@@ -0,0 +1,494 @@
+<template>
+  <div class="tela-senhas-chamadas-com-senha">
+    <div class="rectangle-8"></div>
+    <img class="rectangle-9" src="rectangle-90.svg" />
+    <div class="p-30">P30</div>
+    <div class="senha-em-atendimento">Senha em atendimento</div>
+    <div class="rectangle-14"></div>
+    <div class="ah-325-j-3-w">AH325J3W</div>
+    <div class="c-digo-da-senha">Código da senha:</div>
+    <img class="rectangle-16" src="rectangle-160.svg" />
+    <div class="ellipse-1"></div>
+    <div class="bem-vindo-atendente">Bem vindo Atendente</div>
+    <div class="chame-a-primeira-senha">Chame a primeira senha</div>
+    <div class="rectangle-19"></div>
+    <div class="sair">Sair</div>
+    <img class="vector" src="vector0.svg" />
+    <img
+      class="chat-gpt-image-15-de-mai-de-2026-09-27-35-2"
+      src="chat-gpt-image-15-de-mai-de-2026-09-27-35-20.png"
+    />
+    <div class="rectangle-12"></div>
+    <div class="para-chamar">Para chamar</div>
+    <div class="rectangle-122"></div>
+    <div class="rectangle-11"></div>
+    <div class="rectangle-123"></div>
+    <div class="rectangle-13"></div>
+    <div class="repetir-senha">Repetir senha</div>
+    <div class="chamar-senha">Chamar senha</div>
+    <div class="excluir-senha">Excluir senha</div>
+    <img class="vector2" src="vector1.svg" />
+    <img class="vector3" src="vector2.svg" />
+    <img class="vector4" src="vector3.svg" />
+    <img class="vector5" src="vector4.svg" />
+    <div class="j-atendidos">Já atendidos</div>
+    <img
+      class="chat-gpt-image-21-de-mai-de-2026-08-21-49-1"
+      src="chat-gpt-image-21-de-mai-de-2026-08-21-49-10.png"
+    />
+    <div class="rectangle-24"></div>
+    <div class="rectangle-25"></div>
+    <div class="senha-n-31">Senha: N31</div>
+    <div class="normal">Normal</div>
+    <img
+      class="chat-gpt-image-15-de-mai-de-2026-09-55-06-2"
+      src="chat-gpt-image-15-de-mai-de-2026-09-55-06-20.png"
+    />
+    <img
+      class="chat-gpt-image-15-de-mai-de-2026-09-45-57-2"
+      src="chat-gpt-image-15-de-mai-de-2026-09-45-57-20.png"
+    />
+    <div class="rectangle-31"></div>
+    <div class="senha-p-31">Senha: P31</div>
+    <div class="preferencial">Preferencial</div>
+    <div class="_20-05-2026-s-10-35">| 20/05/2026 ás 10:35</div>
+    <div class="_20-05-2026-s-10-30">| 20/05/2026 ás 10:30</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TelaSenhasChamadasComSenha",
+  components: {},
+  props: {},
+  data() {},
+};
+</script>
+<style scoped>
+.tela-senhas-chamadas-com-senha,
+.tela-senhas-chamadas-com-senha * {
+  box-sizing: border-box;
+}
+.tela-senhas-chamadas-com-senha {
+  background: #ffffff;
+  height: 2048px;
+  position: relative;
+  overflow: hidden;
+}
+.rectangle-8 {
+  background: radial-gradient(
+    closest-side,
+    rgba(240, 243, 252, 1) 0%,
+    rgba(143, 154, 210, 1) 100%
+  );
+  width: 1440px;
+  height: 2048px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+.rectangle-9 {
+  border-radius: 0px;
+  width: 1359px;
+  height: 1614px;
+  position: absolute;
+  left: 40px;
+  top: 382px;
+  overflow: visible;
+}
+.p-30 {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 250px;
+  font-weight: 400;
+  position: absolute;
+  left: 453px;
+  top: 507px;
+}
+.senha-em-atendimento {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 60px;
+  font-weight: 400;
+  position: absolute;
+  left: 61px;
+  top: 408px;
+}
+.rectangle-14 {
+  background: #ccd4ff;
+  border-radius: 25px;
+  width: 462px;
+  height: 106px;
+  position: absolute;
+  left: 453px;
+  top: 866px;
+}
+.ah-325-j-3-w {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  position: absolute;
+  left: 549px;
+  top: 888px;
+}
+.c-digo-da-senha {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 25px;
+  font-weight: 400;
+  position: absolute;
+  left: 453px;
+  top: 836px;
+}
+.rectangle-16 {
+  border-radius: 0px;
+  width: 1359px;
+  height: 251px;
+  position: absolute;
+  left: 40px;
+  top: 68px;
+  overflow: visible;
+}
+.ellipse-1 {
+  background: #8f9ad2;
+  border-radius: 50%;
+  width: 151px;
+  height: 151px;
+  position: absolute;
+  left: 127px;
+  top: 123px;
+}
+.group-13 {
+  position: absolute;
+  inset: 0;
+}
+.bem-vindo-atendente {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  position: absolute;
+  left: 326px;
+  top: 121px;
+}
+.chame-a-primeira-senha {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 326px;
+  top: 183px;
+}
+.rectangle-19 {
+  background: #0f1a52;
+  border-radius: 50px;
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px;
+  width: 157px;
+  height: 56px;
+  position: absolute;
+  left: 1213px;
+  top: 96px;
+}
+.sair {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  position: absolute;
+  left: 1237px;
+  top: 106px;
+}
+.vector {
+  width: 2.85%;
+  height: 1.56%;
+  position: absolute;
+  right: 6.18%;
+  left: 90.97%;
+  bottom: 93.07%;
+  top: 5.37%;
+  overflow: visible;
+}
+.chat-gpt-image-15-de-mai-de-2026-09-27-35-2 {
+  width: 161px;
+  height: 161px;
+  position: absolute;
+  left: 122px;
+  top: 113px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.group-14 {
+  position: absolute;
+  inset: 0;
+}
+.rectangle-12 {
+  background: #ffffff;
+  border-radius: 25px;
+  width: 630.74px;
+  height: 715px;
+  position: absolute;
+  left: 73px;
+  top: 1189px;
+}
+.para-chamar {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 60px;
+  font-weight: 400;
+  position: absolute;
+  left: 103px;
+  top: 1226px;
+  width: 462.28px;
+  height: 97.4px;
+}
+.group-15 {
+  position: absolute;
+  inset: 0;
+}
+.rectangle-122 {
+  background: #ffffff;
+  border-radius: 25px;
+  width: 630.74px;
+  height: 715px;
+  position: absolute;
+  left: 739px;
+  top: 1189px;
+}
+.rectangle-11 {
+  background: #0c56da;
+  border-radius: 25px;
+  width: 410px;
+  height: 80px;
+  position: absolute;
+  left: 73px;
+  top: 1035px;
+}
+.rectangle-123 {
+  background: #e93a32;
+  border-radius: 25px;
+  width: 409px;
+  height: 80px;
+  position: absolute;
+  left: 517px;
+  top: 1035px;
+}
+.rectangle-13 {
+  background: #0c56da;
+  border-radius: 25px;
+  width: 410px;
+  height: 80px;
+  position: absolute;
+  left: 960px;
+  top: 1035px;
+}
+.repetir-senha {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 1039px;
+  top: 1051px;
+}
+.chamar-senha {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 149px;
+  top: 1050px;
+}
+.excluir-senha {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 617px;
+  top: 1051px;
+}
+.vector2 {
+  width: 3.26%;
+  height: 1.86%;
+  position: absolute;
+  right: 28.4%;
+  left: 68.33%;
+  bottom: 46.58%;
+  top: 51.56%;
+  overflow: visible;
+}
+.vector3 {
+  width: 2.5%;
+  height: 1.61%;
+  position: absolute;
+  right: 58.26%;
+  left: 39.24%;
+  bottom: 46.73%;
+  top: 51.66%;
+  overflow: visible;
+}
+.vector4 {
+  width: 3.26%;
+  height: 1.86%;
+  position: absolute;
+  right: 90.21%;
+  left: 6.53%;
+  bottom: 46.58%;
+  top: 51.56%;
+  overflow: visible;
+}
+.vector5 {
+  width: 3.96%;
+  height: 2.15%;
+  position: absolute;
+  right: 61.18%;
+  left: 34.86%;
+  bottom: 37.3%;
+  top: 60.55%;
+  overflow: visible;
+}
+.group-16 {
+  position: absolute;
+  inset: 0;
+}
+.j-atendidos {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 60px;
+  font-weight: 400;
+  position: absolute;
+  left: 800px;
+  top: 1225.67px;
+  width: 462.28px;
+  height: 97.4px;
+}
+.chat-gpt-image-21-de-mai-de-2026-08-21-49-1 {
+  width: 104px;
+  height: 104px;
+  position: absolute;
+  left: 1170px;
+  top: 1210px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.rectangle-24 {
+  background: #000000;
+  width: 560px;
+  height: 1px;
+  position: absolute;
+  left: 100px;
+  top: 1438px;
+}
+.rectangle-25 {
+  background: #ffe2c5;
+  border-radius: 20px;
+  width: 149px;
+  height: 31px;
+  position: absolute;
+  left: 191px;
+  top: 1390px;
+}
+.senha-n-31 {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  position: absolute;
+  left: 213px;
+  top: 1394px;
+}
+.normal {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  position: absolute;
+  left: 191px;
+  top: 1349px;
+}
+.chat-gpt-image-15-de-mai-de-2026-09-55-06-2 {
+  width: 98px;
+  height: 98px;
+  position: absolute;
+  left: 92px;
+  top: 1329px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.chat-gpt-image-15-de-mai-de-2026-09-45-57-2 {
+  width: 81px;
+  height: 81px;
+  position: absolute;
+  left: 100px;
+  top: 1450px;
+  object-fit: cover;
+  aspect-ratio: 1;
+}
+.rectangle-31 {
+  background: #ffe2c5;
+  border-radius: 20px;
+  width: 149px;
+  height: 31px;
+  position: absolute;
+  left: 191px;
+  top: 1491px;
+}
+.senha-p-31 {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  position: absolute;
+  left: 213px;
+  top: 1495px;
+}
+.preferencial {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  position: absolute;
+  left: 191px;
+  top: 1450px;
+}
+._20-05-2026-s-10-35 {
+  color: rgba(0, 0, 0, 0.5);
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 25px;
+  font-weight: 400;
+  position: absolute;
+  left: 374px;
+  top: 1491px;
+}
+._20-05-2026-s-10-30 {
+  color: rgba(0, 0, 0, 0.5);
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 25px;
+  font-weight: 400;
+  position: absolute;
+  left: 374px;
+  top: 1390px;
+}
+</style>
---
+++ figma/usuario/senha_retirar.vue
@@ -0,0 +1,267 @@
+<template>
+  <div class="tela-inicial">
+    <div class="rectangle-3"></div>
+    <div class="rectangle"></div>
+    <div class="sos">SOS</div>
+    <div class="rectangle-6"></div>
+    <div class="rectangle-4"></div>
+    <div class="rectangle-2"></div>
+    <div class="sua-senha-aqui">Sua senha aqui.</div>
+    <div class="preferencial">Preferencial</div>
+    <div class="rectangle-42"></div>
+    <div class="rectangle-22"></div>
+    <div class="sua-senha-aqui2">Sua senha aqui.</div>
+    <div class="normal">Normal</div>
+    <div class="bem-vindo-usu-rio">Bem vindo usuário</div>
+    <div class="retire-sua-senha">retire sua senha</div>
+    <div class="rectangle-11"></div>
+    <div class="entrar-com-o-google">Entrar com o Google</div>
+    <img class="rectangle-12" src="rectangle-120.png" />
+    <div class="line-1"></div>
+    <div class="line-2"></div>
+    <div class="ou">ou</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TelaInicial",
+  components: {},
+  props: {},
+  data() {},
+};
+</script>
+<style scoped>
+.tela-inicial,
+.tela-inicial * {
+  box-sizing: border-box;
+}
+.tela-inicial {
+  background: #ffffff;
+  height: 1024px;
+  position: relative;
+  overflow: hidden;
+}
+.rectangle-3 {
+  background: radial-gradient(
+    closest-side,
+    rgba(240, 243, 252, 1) 0%,
+    rgba(143, 154, 210, 1) 100%
+  );
+  width: 1440px;
+  height: 962px;
+  position: absolute;
+  left: 0px;
+  top: 62px;
+}
+.rectangle {
+  background: #000000;
+  width: 1440px;
+  height: 62px;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+.sos {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  position: absolute;
+  left: 15px;
+  top: 11px;
+}
+.group-10 {
+  position: absolute;
+  inset: 0;
+}
+.rectangle-6 {
+  background: #f0f3fc;
+  border-radius: 25px;
+  width: 1137px;
+  height: 916px;
+  position: absolute;
+  left: 191px;
+  top: 85px;
+}
+.group-19 {
+  position: absolute;
+  inset: 0;
+}
+.rectangle-4 {
+  background: linear-gradient(
+    180deg,
+    rgba(43, 56, 126, 1) 0%,
+    rgba(143, 154, 210, 1) 100%
+  );
+  border-radius: 25px;
+  width: 440px;
+  height: 449px;
+  position: absolute;
+  left: 267px;
+  top: 502px;
+}
+.rectangle-2 {
+  background: #0f1a52;
+  border-radius: 20px;
+  width: 349px;
+  height: 123px;
+  position: absolute;
+  left: 312px;
+  top: 774px;
+}
+.sua-senha-aqui {
+  color: #ffffff;
+  text-align: center;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 316px;
+  top: 812px;
+  width: 349px;
+  height: 47px;
+}
+.preferencial {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 60px;
+  font-weight: 400;
+  position: absolute;
+  left: 312px;
+  top: 593px;
+  width: 408px;
+}
+.group-18 {
+  position: absolute;
+  inset: 0;
+}
+.rectangle-42 {
+  background: linear-gradient(
+    180deg,
+    rgba(143, 154, 210, 1) 0%,
+    rgba(43, 56, 126, 1) 100%
+  );
+  border-radius: 25px;
+  width: 440px;
+  height: 449px;
+  position: absolute;
+  left: 805px;
+  top: 502px;
+}
+.rectangle-22 {
+  background: #0f1a52;
+  border-radius: 20px;
+  width: 344px;
+  height: 123px;
+  position: absolute;
+  left: 853px;
+  top: 774px;
+}
+.sua-senha-aqui2 {
+  color: #ffffff;
+  text-align: center;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 820px;
+  top: 810px;
+  width: 401px;
+  height: 52px;
+}
+.normal {
+  color: #ffffff;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 60px;
+  font-weight: 400;
+  position: absolute;
+  left: 922px;
+  top: 590px;
+  width: 303px;
+  height: 61px;
+}
+.bem-vindo-usu-rio {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  position: absolute;
+  left: 542px;
+  top: 102px;
+}
+.retire-sua-senha {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 50px;
+  font-weight: 400;
+  position: absolute;
+  left: 569px;
+  top: 163px;
+}
+.rectangle-11 {
+  background: #f2f2f2;
+  border-radius: 50px;
+  width: 623px;
+  height: 106px;
+  position: absolute;
+  left: 448px;
+  top: 256px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+}
+.entrar-com-o-google {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 40px;
+  font-weight: 400;
+  position: absolute;
+  left: 566px;
+  top: 285px;
+}
+.rectangle-12 {
+  border-radius: 100px;
+  width: 60px;
+  height: 62px;
+  position: absolute;
+  left: 491px;
+  top: 278px;
+  object-fit: cover;
+}
+.line-1 {
+  margin-top: -1px;
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px 0 0 0;
+  width: 488px;
+  height: 0px;
+  position: absolute;
+  left: 232px;
+  top: 435px;
+}
+.line-2 {
+  margin-top: -1px;
+  border-style: solid;
+  border-color: #000000;
+  border-width: 1px 0 0 0;
+  width: 488px;
+  height: 0px;
+  position: absolute;
+  left: 805px;
+  top: 435px;
+}
+.ou {
+  color: #000000;
+  text-align: left;
+  font-family: "Inter-Regular", sans-serif;
+  font-size: 30px;
+  font-weight: 400;
+  position: absolute;
+  left: 742px;
+  top: 417px;
+}
+</style>
